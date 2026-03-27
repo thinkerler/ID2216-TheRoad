cdfaae83d1e133ffba6e23aa9ce3c4eea70ffe87
Chaoqi <xuchaoqi550@gmail.com>
2026-03-27 01:12:50 +0100

    Add files via upload
    change the index page

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,22 +1,3 @@
-import { View, Text } from "react-native";
-import { Colors, Typography, CommonStyles } from "../shared/theme";
+import HubScreen from '../hub/view/HubScreen';
 
-export default function HubScreen() {
-  return (
-    <View style={CommonStyles.screenPadded}>
-      <View style={CommonStyles.appHeader}>
-        <Text style={CommonStyles.appHeaderTitle}>The Road Goes Ever On</Text>
-        <Text style={CommonStyles.appHeaderSubtitle}>All the World's a Road</Text>
-      </View>
-
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ ...Typography.pageTitle, color: Colors.textPrimary }}>
-          Hub
-        </Text>
-        <Text style={{ ...Typography.body, color: Colors.textSecondary, marginTop: 8 }}>
-          Globe & Time Machine
-        </Text>
-      </View>
-    </View>
-  );
-}
+export default HubScreen;
